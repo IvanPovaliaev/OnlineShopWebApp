54d8d8f808d84d58084880fb85d17fef9bc00695
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-08 18:17:27 +0300

    OnlineShopWebApp 1. Добавлен ProductController 2. Реализована логика получения информации об одном товаре

---
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System.Linq;
+
+namespace OnlineShopWebApp.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        public ProductController() { }
+
+        [Route("index/{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = new TempProductsStorage().GetAll()
+                .FirstOrDefault(p => p.Id == id);
+
+            if (product == null) return NotFound($"Товар с ID:{id} не найден");
+
+            var productInfo = $"{product.Id}\n{product.Name}\n{product.Cost}\n{product.Description}\n{product.Category}";
+
+            var productSpecificationsText = product.Specifications.Select(spec => $"{spec.Key}: {spec.Value}");
+
+            var requestAnswer = $"{productInfo}\n\nХарактеристики:\n{string.Join("\n",productSpecificationsText)}";
+
+            return Ok(requestAnswer);
+        }
+    }
+}
