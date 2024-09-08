5cc8845ed4b9ad42f6661084c80a3c0aa31619a5
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-08 17:38:21 +0300

    OnlineShopWebApp: 1. Сделал usings видимыми 2. Добавлена модель Product и перечисление ProductCategories 3. В HomeController добавлено отображение списка товаров

---
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+        public ProductCategories Category { get; set; }
+
+        //Необходимые характеристики будут определяться на фронте в зависимости от категории
+        public Dictionary<string, string> Specifications { get; set; }
+
+        public Product()
+        {
+            Specifications = [];
+        }
+
+        public Product(int id, string name, decimal cost, string description, ProductCategories category) : this()
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+            Description = description;
+            Category = category;
+        }
+    }
+}
