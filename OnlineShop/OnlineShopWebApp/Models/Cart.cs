65dfba52fee68be7a82e101cc1f450c9c1d81d90
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-15 20:12:39 +0300

    1. Реализовал базовую логику работы с корзиной

---
+++ OnlineShop/OnlineShopWebApp/Models/Cart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; } //Для дальнейшей привязки пользователя
+        public List<CartPosition> Positions { get; set; }
+
+        public decimal TotalCost
+        {
+            get => Positions.Sum(p => p.Cost);
+        }
+
+        public Cart()
+        {
+            Id = Guid.NewGuid();
+            Positions = [];
+        }
+
+        public Cart(Guid userId) : this()
+        {
+            UserId = userId;
+        }
+    }
+}
