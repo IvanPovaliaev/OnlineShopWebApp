193c260c98ee881eb7f330ae5029834549222a64
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-15 20:12:39 +0300

    1. Реализовал базовую логику работы с корзиной

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Services;
+using System;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        private Guid _userId = new Guid("74f1f6b5-083a-4677-8f68-8255caa77965"); //Временный guid для тестирования
+        private CartsService _cartsService;
+        private ProductsService _productsService;
+
+        public CartController()
+        {
+            _cartsService = new CartsService();
+            _productsService = new ProductsService();
+        }
+
+        public IActionResult Index()
+        {
+            var cart = _cartsService.Get(_userId);
+            return View(cart);
+        }
+
+        public IActionResult Add(Guid productId)
+        {
+            var product = _productsService.Get(productId);
+            _cartsService.Add(product, _userId);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Services/CartsService.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShopWebApp.Services
+{
+    public class CartsService
+    {
+        public const string FilePath = @".\Data\Carts.json";
+        private List<Cart> _carts;
+
+        public CartsService()
+        {
+            UpdatesCarts();
+        }
+
+        public Cart Get(Guid userId)
+        {
+            return _carts.FirstOrDefault(cart => cart.UserId == userId);
+        }
+
+        public void Add(Product product, Guid userId)
+        {
+            var userCart = Get(userId);
+
+            if (userCart is null)
+            {
+                CreateNewCart(product, userId);
+                return;
+            }
+
+            var cartPosition = userCart.Positions
+                .FirstOrDefault(position => position.Product.Id == product.Id);
+
+            if (cartPosition is null)
+            {
+                AddNewPosition(userCart, product);
+                return;
+            }
+
+            cartPosition.Quantity++;
+            Save();
+        }
+
+        private void CreateNewCart(Product product, Guid userId)
+        {
+            var cart = new Cart(userId);
+            AddNewPosition(cart, product);
+            _carts.Add(cart);
+            Save();
+        }
+
+        private void AddNewPosition(Cart cart, Product product)
+        {
+            var newPosition = new CartPosition(product, 1);
+            cart.Positions.Add(newPosition);
+            Save();
+        }
+
+        private void Save()
+        {
+            var jsonData = JsonConvert.SerializeObject(_carts, Formatting.Indented);
+            FileService.Save(FilePath, jsonData);
+        }
+
+        private void UpdatesCarts()
+        {
+            var cartsJson = FileService.GetContent(FilePath);
+
+            if (cartsJson is null)
+            {
+                _carts = [];
+                return;
+            }
+
+            _carts = JsonConvert.DeserializeObject<List<Cart>>(cartsJson);
+        }
+    }
+}
