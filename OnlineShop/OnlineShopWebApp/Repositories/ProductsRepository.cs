b482d780a9986eb5c85c3d9d60f14cb37f18bd9f
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-21 11:34:18 +0300

    Задокументировал методы в ProductsRepository

--- OnlineShop/OnlineShopWebApp/Repositories/ProductsRepository.cs
+++ OnlineShop/OnlineShopWebApp/Repositories/ProductsRepository.cs
@@ -17,26 +17,44 @@ namespace OnlineShopWebApp.Repositories
             Upload();
         }
 
+        /// <summary>
+        /// Get all products
+        /// </summary>
+        /// <returns>List of all products</returns>
         public List<Product> GetAll() => _products;
 
+        /// <summary>
+        /// Get product by GUID
+        /// </summary>
+        /// <returns>Product; returns null if product not found</returns>
         public Product Get(Guid id)
         {
             var product = _products.FirstOrDefault(p => p.Id == id);
             return product;
         }
 
-        public void Add(IEnumerable<Product> products)
+        /// <summary>
+        /// Add list of products
+        /// </summary>
+        /// <param name="products">Products list</param>
+        public void Add(List<Product> products)
         {
             _products.AddRange(products);
             SaveChanges();
         }
 
+        /// <summary>
+        /// Save changes to storage
+        /// </summary>
         private void SaveChanges()
         {
             var jsonData = JsonConvert.SerializeObject(_products, Formatting.Indented);
             FileService.Save(FilePath, jsonData);
         }
 
+        /// <summary>
+        /// Upload products
+        /// </summary>   
         private void Upload()
         {
             if (!FileService.Exists(FilePath) || string.IsNullOrEmpty(FileService.GetContent(FilePath)))
