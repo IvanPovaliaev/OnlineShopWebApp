313c8d74eff47a5ccb034e6b97c142234247a16d
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-22 21:14:33 +0300

    1. Добавил формы для ввода данных заказа 2. Создал OrderController и Order

---
+++ OnlineShop/OnlineShopWebApp/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        private Guid _userId = new Guid("74f1f6b5-083a-4677-8f68-8255caa77965"); //Временный guid для тестирования
+
+        [HttpPost]
+        public IActionResult Register(Order order)
+        {
+            order.UserId = _userId;
+            return View(order);
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string City { get; set; }
+        public string Adress { get; set; }
+        public int? Entrance { get; set; }
+        public int? Floor { get; set; }
+        public int? Apartament { get; set; }
+        public int Index { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string? ReservePhone { get; set; }
+        public string AdditionalInfo { get; set; }
+
+        public Order()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.Now;
+        }
+    }
+}
