19a1888cabb89d6aedea0ed2ff9f3f2ae89630d5
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-22 12:24:35 +0300

    1. Выделил интерфейсы для классов-хранилищ. 2. Выделил отдельную папку для интерфейсов 3. Перенёс документирование необходимых методов из хранилища в интерфейсы

---
+++ OnlineShop/OnlineShopWebApp/Interfaces/ICartsRepository.cs
@@ -0,0 +1,25 @@
+﻿using OnlineShopWebApp.Models;
+using System;
+
+namespace OnlineShopWebApp.Interfaces
+{
+    public interface ICartsRepository
+    {
+        /// <summary>
+        /// Get cart by userId (guid)
+        /// </summary>        
+        /// <returns>Cart for related user</returns>
+        /// <param name="userId">GUID user id</param>
+        Cart Get(Guid userId);
+
+        /// <summary>
+        /// Create a new cart
+        /// </summary>   
+        void Create(Cart cart);
+
+        /// <summary>
+        /// Update cart with identical id. If cart is not in the repository - does nothing.
+        /// </summary>  
+        void Update(Cart cart);
+    }
+}
