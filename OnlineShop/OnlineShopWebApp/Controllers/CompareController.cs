a476ebd961e3db532f07dbe72f763cdedb667337
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-24 00:46:37 +0300

    Начал работу над страницей сравнения. 1. Добавил CompareController 2. Добавил соответствующее представление 3. Добавил тестовую логику

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CompareController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Services;
+using System;
+using System.Linq;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CompareController : Controller
+    {
+        private Guid _userId = new Guid("74f1f6b5-083a-4677-8f68-8255caa77965"); //Временный guid для тестирования
+        private readonly ProductsService _productsService;
+        private readonly CartsService _cartsService;
+
+        public CompareController(ProductsService productsService, CartsService cartsService)
+        {
+            _productsService = productsService;
+            _cartsService = cartsService;
+        }
+
+        /// <summary>
+        /// Open compare page
+        /// </summary>
+        /// <returns>Compare page View</returns>
+        public IActionResult Index()
+        {
+            var products = _productsService.GetAll();
+            var groupProducts = products.ToLookup(pr => pr.Category);
+
+            return View(groupProducts);
+        }
+    }
+}
