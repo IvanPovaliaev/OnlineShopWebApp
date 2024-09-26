f4da8aba0911c74677329fe6f1e7bee99c34899c
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-26 19:49:14 +0300

    Создал IFavoritesRepository, InFileFavoritesRepository, FavoriteProduct

---
+++ OnlineShop/OnlineShopWebApp/Models/FavoriteProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OnlineShopWebApp.Models
+{
+    public class FavoriteProduct
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; } //Для дальнейшей привязки пользователя
+        public Product Product { get; set; }
+
+        public FavoriteProduct(Guid userId, Product product)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Product = product;
+        }
+    }
+}
