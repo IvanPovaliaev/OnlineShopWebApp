21f7393efa48ee8c8bcdd74558eb9905d346514b
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-27 23:18:45 +0300

    Удалил закомментированный код Добавил компонент для сравнения

---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Comparison/ComparisonViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Services;
+using System;
+
+namespace OnlineShopWebApp.Views.Shared.Components.Cart
+{
+    public class ComparisonViewComponent : ViewComponent
+    {
+        private Guid _userId = new Guid("74f1f6b5-083a-4677-8f68-8255caa77965"); //Временный guid для тестирования
+        private readonly ComparisonsService _comparisonsService;
+
+        public ComparisonViewComponent(ComparisonsService comparisonsService)
+        {
+            _comparisonsService = comparisonsService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var comparisonsCount = _comparisonsService
+                .GetAll(_userId)
+                .Count;
+            return View("Comparison", comparisonsCount);
+        }
+    }
+}
