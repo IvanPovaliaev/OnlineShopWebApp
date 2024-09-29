260f492103abfaff5c94adda22926492767b328d
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-29 23:46:43 +0300

    1. Добавил недостающие методы в AdminController 2. Добавил недостающие представления

--- OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
@@ -8,8 +8,34 @@ namespace OnlineShopWebApp.Controllers
 		/// Open Admin Order Page
 		/// </summary>
 		/// <returns>Admin Order View</returns>
-
 		public IActionResult Order()
+		{
+			return View();
+		}
+
+		/// <summary>
+		/// Open Admin Users Page
+		/// </summary>
+		/// <returns>Admin Users View</returns>
+		public IActionResult Users()
+		{
+			return View();
+		}
+
+		/// <summary>
+		/// Open Admin Roles Page
+		/// </summary>
+		/// <returns>Admin Roles View</returns>
+		public IActionResult Roles()
+		{
+			return View();
+		}
+
+		/// <summary>
+		/// Open Admin Products Page
+		/// </summary>
+		/// <returns>Admin Producs View</returns>
+		public IActionResult Products()
 		{
 			return View();
 		}
