51d2dc350e61bda009b000d326ade486ab3916db
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-29 19:00:42 +0300

    Создал UserController Login Представления.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+        }
+
+        /// <summary>
+        /// Login as user
+        /// </summary>
+        /// <returns>Home page</returns>
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
