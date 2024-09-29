8590b8653ae2c5bd6eb8b3afbf80ceec3923b1c1
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-09-29 23:42:27 +0300

    1. Создан AdminController 2. Добавлены представление для Order

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+	public class AdminController : Controller
+	{
+		/// <summary>
+		/// Open Admin Order Page
+		/// </summary>
+		/// <returns>Admin Order View</returns>
+
+		public IActionResult Order()
+		{
+			return View();
+		}
+	}
+}
