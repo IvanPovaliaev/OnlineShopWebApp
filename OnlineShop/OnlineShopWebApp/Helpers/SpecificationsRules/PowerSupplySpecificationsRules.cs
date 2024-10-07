4f8cfb6fb4c8a250c4e6a1d96cec945eab30fa60
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-10-08 00:58:42 +0300

    Завершил паттерны для характеристик

--- OnlineShop/OnlineShopWebApp/Helpers/SpecificationsRules/PowerSupplySpecificationsRules.cs
+++ OnlineShop/OnlineShopWebApp/Helpers/SpecificationsRules/PowerSupplySpecificationsRules.cs
@@ -8,14 +8,17 @@ namespace OnlineShopWebApp.Helpers.SpecificationsRules
         public List<ProductSpecificationRule> GetAll()
         {
             var powerPattern = @"^\d+\sВт$";
-            var powerPatternErrorMessage = @"Неверная мощность. Пример: 600 Вт.";
+            var powerPatternErrorMessage = @"Неверная мощность. Пример: 600 Вт";
+
+            var pfcPattern = @"^(активный|пассивный)$";
+            var pfcErrorMessage = @"Неверный тип. Доступные варианты: активный; пассивный";
 
             var rules = new List<ProductSpecificationRule>
                 {
                     new("Manufacturer"),
                     new("ManufacturerCode"),
                     new("Power", powerPattern, powerPatternErrorMessage),
-                    new("PFC"),
+                    new("PFC", pfcPattern, pfcErrorMessage),
                     new("FanSize")
                 };
 
--- OnlineShop/OnlineShopWebApp/Helpers/SpecificationsRules/ProcessorSpecificationsRules.cs
+++ OnlineShop/OnlineShopWebApp/Helpers/SpecificationsRules/ProcessorSpecificationsRules.cs
@@ -7,16 +7,28 @@ namespace OnlineShopWebApp.Helpers.SpecificationsRules
     {
         public List<ProductSpecificationRule> GetAll()
         {
+            var manufacturerPattern = @"^(AMD|Intel)$";
+            var manufacturerErrorMessage = @"Неверный производитель. Доступные варианты: AMD; Intel";
+
+            var сoresCountPattern = @"^\d+$";
+            var сoresCountErrorMessage = @"Неверное количество ядер. Пример: 1; 2; 3; 4; 8";
+
+            var threadsCountPattern = @"^\d+$";
+            var threadsCountErrorMessage = @"Неверное количество потоков. Пример: 1; 2; 3; 4; 8";
+
+            var clockSpeedPattern = @"^\d+\sМГц$";
+            var clockSpeedErrorMessage = @"Неверная тактовая частота. Пример: 3200 МГц";
+
             var rules = new List<ProductSpecificationRule>
                 {
-                    new("Manufacturer"),
+                    new("Manufacturer", manufacturerPattern, manufacturerErrorMessage),
                     new("ManufacturerCode"),
                     new("Model"),
                     new("Socket"),
                     new("Architecture"),
-                    new("CoresCount"),
-                    new("ThreadsCount"),
-                    new("ClockSpeed")
+                    new("CoresCount", сoresCountPattern, сoresCountErrorMessage),
+                    new("ThreadsCount", threadsCountPattern, threadsCountErrorMessage),
+                    new("ClockSpeed", clockSpeedPattern, clockSpeedErrorMessage)
                 };
 
             return rules;
