39331ab6587ab92e046a69d143791a3e150deacf
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-10-18 02:09:43 +0300

    Удалил лишнии классы, оставшиеся после варианта с рефлексией.

--- OnlineShop/OnlineShopWebApp/Helpers/ExcludeFromExcelExportAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace OnlineShopWebApp.Helpers
-{
-    [AttributeUsage(AttributeTargets.Property)]
-    public class ExcludeFromExcelExportAttribute : Attribute
-    {
-    }
-}
