4eb4b8b4ef0d9f169d75a596a974f84090a96058
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-11-10 23:09:29 +0300

    Небольшой рефакторинг

--- OnlineShop/OnlineShop.Db/Constants.cs
+++ OnlineShop/OnlineShop.Db/Constants.cs
@@ -3,6 +3,8 @@
     public static class Constants
     {
         public const string AdminRoleName = "Admin";
+        public const string AdminEmail = "PCDdream@gmail.com";
+        public const string AdminPassword = "Qwerty123!";
         public const string UserRoleName = "User";
     }
 }
