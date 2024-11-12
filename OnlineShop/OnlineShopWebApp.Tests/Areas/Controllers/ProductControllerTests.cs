3cb0a6b96cc84b9a26b0961d9bc94088bbcf31d6
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-11-13 00:19:20 +0300

    Восстановлены тесты для 1. Контроллеров зоны админа: OrderController и ProductController 2. RolesSelectionViewComponents

--- OnlineShop/OnlineShopWebApp.Tests/Areas/Controllers/ProductControllerTests.cs
+++ OnlineShop/OnlineShopWebApp.Tests/Areas/Controllers/ProductControllerTests.cs
@@ -1,205 +1,202 @@
-﻿//using AutoMapper;
-//using Bogus;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.ModelBinding;
-//using Moq;
-//using OnlineShop.Db;
-//using OnlineShop.Db.Models;
-//using OnlineShopWebApp.Areas.Admin.Controllers;
-//using OnlineShopWebApp.Areas.Admin.Models;
-//using OnlineShopWebApp.Helpers;
-//using OnlineShopWebApp.Models;
-//using OnlineShopWebApp.Services;
-//using OnlineShopWebApp.Tests.Helpers;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using AutoMapper;
+using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Moq;
+using OnlineShop.Db.Models;
+using OnlineShopWebApp.Areas.Admin.Controllers;
+using OnlineShopWebApp.Areas.Admin.Models;
+using OnlineShopWebApp.Helpers;
+using OnlineShopWebApp.Models;
+using OnlineShopWebApp.Services;
+using OnlineShopWebApp.Tests.Helpers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace OnlineShopWebApp.Tests.Areas.Controllers
-//{
-//    public class ProductControllerTests
-//    {
-//        private readonly Mock<ProductsService> _productsServiceMock;
-//        private readonly ProductController _controller;
-//        private readonly IMapper _mapper;
+namespace OnlineShopWebApp.Tests.Areas.Controllers
+{
+    public class ProductControllerTests
+    {
+        private readonly Mock<ProductsService> _productsServiceMock;
+        private readonly ProductController _controller;
+        private readonly IMapper _mapper;
 
-//        private readonly Faker<Product> _productFaker;
-//        private readonly List<Product> _fakeProducts;
+        private readonly Faker<Product> _productFaker;
+        private readonly List<Product> _fakeProducts;
 
-//        public ProductControllerTests()
-//        {
-//            _productsServiceMock = new Mock<ProductsService>(null!, null!, null!, null!);
-//            _controller = new ProductController(_productsServiceMock.Object);
+        public ProductControllerTests(IMapper mapper, FakerProvider fakerProvider)
+        {
+            _productsServiceMock = new Mock<ProductsService>(null!, null!, null!, null!);
+            _controller = new ProductController(_productsServiceMock.Object);
 
-//            var config = new MapperConfiguration(cfg => cfg.AddProfile<TestMappingProfile>());
-//            _mapper = config.CreateMapper();
+            _mapper = mapper;
+            _productFaker = fakerProvider.ProductFaker;
+            _fakeProducts = fakerProvider.FakeProducts;
+        }
 
-//            _productFaker = FakerProvider.ProductFaker;
-//            _fakeProducts = FakerProvider.FakeProducts;
-//        }
+        [Fact]
+        public async Task Index_WhenCalled_ReturnsViewWithProducts()
+        {
+            // Arrange
+            var fakeProducts = _fakeProducts.Select(_mapper.Map<ProductViewModel>)
+                                            .ToList();
+            _productsServiceMock.Setup(s => s.GetAllAsync())
+                                .ReturnsAsync(fakeProducts);
 
-//        [Fact]
-//        public async Task Index_WhenCalled_ReturnsViewWithProducts()
-//        {
-//            // Arrange
-//            var fakeProducts = _fakeProducts.Select(_mapper.Map<ProductViewModel>)
-//                                            .ToList();
-//            _productsServiceMock.Setup(s => s.GetAllAsync())
-//                                .ReturnsAsync(fakeProducts);
+            // Act
+            var result = await _controller.Index();
 
-//            // Act
-//            var result = await _controller.Index();
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<List<ProductViewModel>>(viewResult.Model);
+            Assert.Equal(fakeProducts, model);
+        }
 
-//            // Assert
-//            var viewResult = Assert.IsType<ViewResult>(result);
-//            var model = Assert.IsAssignableFrom<List<ProductViewModel>>(viewResult.Model);
-//            Assert.Equal(fakeProducts, model);
-//        }
+        [Fact]
+        public void Add_GetRequest_ReturnsView()
+        {
+            // Act
+            var result = _controller.Add();
 
-//        [Fact]
-//        public void Add_GetRequest_ReturnsView()
-//        {
-//            // Act
-//            var result = _controller.Add();
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
 
-//            // Assert
-//            Assert.IsType<ViewResult>(result);
-//        }
+        [Fact]
+        public async Task Add_PostRequest_WithInvalidModel_ReturnsViewWithModel()
+        {
+            // Arrange
+            var invalidProduct = new AddProductViewModel();
+            _controller.ModelState.AddModelError("Name", "Required");
 
-//        [Fact]
-//        public async Task Add_PostRequest_WithInvalidModel_ReturnsViewWithModel()
-//        {
-//            // Arrange
-//            var invalidProduct = new AddProductViewModel();
-//            _controller.ModelState.AddModelError("Name", "Required");
+            // Act
+            var result = await _controller.Add(invalidProduct);
 
-//            // Act
-//            var result = await _controller.Add(invalidProduct);
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Add", viewResult.ViewName);
+            Assert.Equal(invalidProduct, viewResult.Model);
+        }
 
-//            // Assert
-//            var viewResult = Assert.IsType<ViewResult>(result);
-//            Assert.Equal("Add", viewResult.ViewName);
-//            Assert.Equal(invalidProduct, viewResult.Model);
-//        }
+        [Fact]
+        public async Task Add_PostRequest_WithValidModel_AddsProductAndRedirectsToIndex()
+        {
+            // Arrange
+            var newProduct = _mapper.Map<AddProductViewModel>(_productFaker.Generate());
 
-//        [Fact]
-//        public async Task Add_PostRequest_WithValidModel_AddsProductAndRedirectsToIndex()
-//        {
-//            // Arrange
-//            var newProduct = _mapper.Map<AddProductViewModel>(_productFaker.Generate());
+            // Act
+            var result = await _controller.Add(newProduct);
 
-//            // Act
-//            var result = await _controller.Add(newProduct);
+            // Assert
+            _productsServiceMock.Verify(s => s.AddAsync(newProduct), Times.Once);
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+        }
 
-//            // Assert
-//            _productsServiceMock.Verify(s => s.AddAsync(newProduct), Times.Once);
-//            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
-//            Assert.Equal("Index", redirectResult.ActionName);
-//        }
+        [Fact]
+        public async Task Edit_WhenCalled_ReturnsViewWithProduct()
+        {
+            // Arrange
+            var fakeProduct = _fakeProducts.First();
+            var fakeEditProduct = _mapper.Map<EditProductViewModel>(fakeProduct);
+            _productsServiceMock.Setup(s => s.GetEditProductAsync(fakeProduct.Id))
+                                .ReturnsAsync(fakeEditProduct);
 
-//        [Fact]
-//        public async Task Edit_WhenCalled_ReturnsViewWithProduct()
-//        {
-//            // Arrange
-//            var fakeProduct = _fakeProducts.First();
-//            var fakeEditProduct = _mapper.Map<EditProductViewModel>(fakeProduct);
-//            _productsServiceMock.Setup(s => s.GetEditProductAsync(fakeProduct.Id))
-//                                .ReturnsAsync(fakeEditProduct);
+            // Act
+            var result = await _controller.Edit(fakeProduct.Id);
 
-//            // Act
-//            var result = await _controller.Edit(fakeProduct.Id);
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<EditProductViewModel>(viewResult.Model);
+            Assert.Equal(fakeEditProduct, model);
+        }
 
-//            // Assert
-//            var viewResult = Assert.IsType<ViewResult>(result);
-//            var model = Assert.IsType<EditProductViewModel>(viewResult.Model);
-//            Assert.Equal(fakeEditProduct, model);
-//        }
+        [Fact]
+        public async Task Update_WithInvalidModel_ReturnsEditViewWithModel()
+        {
+            // Arrange
+            var invalidProduct = new EditProductViewModel();
+            var modelState = _controller.ModelState;
+            modelState.AddModelError("Name", "Required");
+            _productsServiceMock.Setup(s => s.IsUpdateValidAsync(modelState, invalidProduct))
+                                .ReturnsAsync(false);
 
-//        [Fact]
-//        public async Task Update_WithInvalidModel_ReturnsEditViewWithModel()
-//        {
-//            // Arrange
-//            var invalidProduct = new EditProductViewModel();
-//            var modelState = _controller.ModelState;
-//            modelState.AddModelError("Name", "Required");
-//            _productsServiceMock.Setup(s => s.IsUpdateValidAsync(modelState, invalidProduct))
-//                                .ReturnsAsync(false);
+            // Act
+            var result = await _controller.Update(invalidProduct);
 
-//            // Act
-//            var result = await _controller.Update(invalidProduct);
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Edit", viewResult.ViewName);
+            Assert.Equal(invalidProduct, viewResult.Model);
+        }
 
-//            // Assert
-//            var viewResult = Assert.IsType<ViewResult>(result);
-//            Assert.Equal("Edit", viewResult.ViewName);
-//            Assert.Equal(invalidProduct, viewResult.Model);
-//        }
+        [Fact]
+        public async Task Update_WithValidModel_UpdatesProductAndRedirectsToIndex()
+        {
+            // Arrange
+            var fakeProduct = _fakeProducts.First();
+            var fakeEditProduct = _mapper.Map<EditProductViewModel>(fakeProduct);
+            var modelState = new ModelStateDictionary();
 
-//        [Fact]
-//        public async Task Update_WithValidModel_UpdatesProductAndRedirectsToIndex()
-//        {
-//            // Arrange
-//            var fakeProduct = _fakeProducts.First();
-//            var fakeEditProduct = _mapper.Map<EditProductViewModel>(fakeProduct);
-//            var modelState = new ModelStateDictionary();
+            _productsServiceMock.Setup(s => s.IsUpdateValidAsync(modelState, fakeEditProduct))
+                                .ReturnsAsync(true);
 
-//            _productsServiceMock.Setup(s => s.IsUpdateValidAsync(modelState, fakeEditProduct))
-//                                .ReturnsAsync(true);
+            // Act
+            var result = await _controller.Update(fakeEditProduct);
 
-//            // Act
-//            var result = await _controller.Update(fakeEditProduct);
+            // Assert
+            _productsServiceMock.Verify(s => s.UpdateAsync(fakeEditProduct), Times.Once);
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+        }
 
-//            // Assert
-//            _productsServiceMock.Verify(s => s.UpdateAsync(fakeEditProduct), Times.Once);
-//            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
-//            Assert.Equal("Index", redirectResult.ActionName);
-//        }
+        [Fact]
+        public async Task Delete_WhenCalled_DeletesProductAndRedirectsToIndex()
+        {
+            // Arrange
+            var productId = _fakeProducts.First().Id;
 
-//        [Fact]
-//        public async Task Delete_WhenCalled_DeletesProductAndRedirectsToIndex()
-//        {
-//            // Arrange
-//            var productId = _fakeProducts.First().Id;
+            // Act
+            var result = await _controller.Delete(productId);
 
-//            // Act
-//            var result = await _controller.Delete(productId);
+            // Assert
+            _productsServiceMock.Verify(s => s.DeleteAsync(productId), Times.Once);
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+        }
 
-//            // Assert
-//            _productsServiceMock.Verify(s => s.DeleteAsync(productId), Times.Once);
-//            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
-//            Assert.Equal("Index", redirectResult.ActionName);
-//        }
+        [Fact]
+        public void GetSpecificationsForm_WhenCalled_ReturnsSpecificationsFormViewComponent()
+        {
+            // Arrange
+            var category = (ProductCategoriesViewModel)_fakeProducts.First().Category;
 
-//        [Fact]
-//        public void GetSpecificationsForm_WhenCalled_ReturnsSpecificationsFormViewComponent()
-//        {
-//            // Arrange
-//            var category = (ProductCategoriesViewModel)_fakeProducts.First().Category;
+            // Act
+            var result = _controller.GetSpecificationsForm(category);
 
-//            // Act
-//            var result = _controller.GetSpecificationsForm(category);
+            // Assert
+            var viewComponentResult = Assert.IsType<ViewComponentResult>(result);
+            var model = Assert.IsType<(Dictionary<string, string>, ProductCategoriesViewModel)>(viewComponentResult.Arguments);
+            Assert.Equal(category, model.Item2);
+        }
 
-//            // Assert
-//            var viewComponentResult = Assert.IsType<ViewComponentResult>(result);
-//            var model = Assert.IsType<(Dictionary<string, string>, ProductCategoriesViewModel)>(viewComponentResult.Arguments);
-//            Assert.Equal(category, model.Item2);
-//        }
+        [Fact]
+        public async Task ExportToExcel_WhenCalled_ReturnsExcelFileStream()
+        {
+            // Arrange
+            var fakeStream = new MemoryStream();
+            _productsServiceMock.Setup(s => s.ExportAllToExcelAsync())
+                                .ReturnsAsync(fakeStream);
 
-//        [Fact]
-//        public async Task ExportToExcel_WhenCalled_ReturnsExcelFileStream()
-//        {
-//            // Arrange
-//            var fakeStream = new MemoryStream();
-//            _productsServiceMock.Setup(s => s.ExportAllToExcelAsync())
-//                                .ReturnsAsync(fakeStream);
+            // Act
+            var result = await _controller.ExportToExcel();
 
-//            // Act
-//            var result = await _controller.ExportToExcel();
-
-//            // Assert
-//            var fileResult = Assert.IsType<FileStreamResult>(result);
-//            Assert.Equal(Constants.ExcelFileContentType, fileResult.ContentType);
-//        }
-//    }
-//}
+            // Assert
+            var fileResult = Assert.IsType<FileStreamResult>(result);
+            Assert.Equal(Formats.ExcelFileContentType, fileResult.ContentType);
+        }
+    }
+}
--- OnlineShop/OnlineShopWebApp.Tests/Areas/Views/User/Components/RolesSelection/RolesSelectionViewComponentsTests.cs
+++ OnlineShop/OnlineShopWebApp.Tests/Areas/Views/User/Components/RolesSelection/RolesSelectionViewComponentsTests.cs
@@ -1,79 +1,75 @@
-﻿//using AutoMapper;
-//using Microsoft.AspNetCore.Mvc.ViewComponents;
-//using Moq;
-//using OnlineShop.Db;
-//using OnlineShopWebApp.Areas.Admin.Models;
-//using OnlineShopWebApp.Areas.Admin.Views.User.Components.RolesSelection;
-//using OnlineShopWebApp.Helpers;
-//using OnlineShopWebApp.Services;
-//using OnlineShopWebApp.Tests.Helpers;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using OnlineShop.Db;
+using OnlineShopWebApp.Areas.Admin.Models;
+using OnlineShopWebApp.Areas.Admin.Views.User.Components.RolesSelection;
+using OnlineShopWebApp.Services;
+using OnlineShopWebApp.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace OnlineShopWebApp.Tests.Areas.Views.User.Components.RolesSelection
-//{
-//    public class RolesSelectionViewComponentTests
-//    {
-//        private readonly Mock<RolesService> _rolesServiceMock;
-//        private readonly RolesSelectionViewComponent _viewComponent;
-//        private readonly List<RoleViewModel> _fakeRoles;
+namespace OnlineShopWebApp.Tests.Areas.Views.User.Components.RolesSelection
+{
+    public class RolesSelectionViewComponentTests
+    {
+        private readonly Mock<RolesService> _rolesServiceMock;
+        private readonly RolesSelectionViewComponent _viewComponent;
+        private readonly List<RoleViewModel> _fakeRoles;
 
-//        public RolesSelectionViewComponentTests()
-//        {
-//            _rolesServiceMock = new Mock<RolesService>(null!, null!, null!, null!);
-//            _viewComponent = new RolesSelectionViewComponent(_rolesServiceMock.Object);
+        public RolesSelectionViewComponentTests(IMapper mapper, FakerProvider fakerProvider)
+        {
+            _rolesServiceMock = new Mock<RolesService>(null!, null!, null!, null!);
+            _viewComponent = new RolesSelectionViewComponent(_rolesServiceMock.Object);
 
-//            var config = new MapperConfiguration(cfg => cfg.AddProfile<TestMappingProfile>());
-//            var mapper = config.CreateMapper();
+            _fakeRoles = fakerProvider.FakeRoles.Select(mapper.Map<RoleViewModel>)
+                                                .ToList();
+        }
 
-//            _fakeRoles = FakerProvider.FakeRoles.Select(mapper.Map<RoleViewModel>)
-//                                                .ToList();
-//        }
+        [Fact]
+        public async Task InvokeAsync_WhenSelectedRoleExists_ReturnsViewWithSelectedRoleName()
+        {
+            // Arrange
+            var selectedRoleName = _fakeRoles.First().Name;
 
-//        [Fact]
-//        public async Task InvokeAsync_WhenSelectedRoleExists_ReturnsViewWithSelectedRole()
-//        {
-//            // Arrange
-//            var selectedRoleId = _fakeRoles.First().Id;
+            _rolesServiceMock.Setup(r => r.GetAllAsync())
+                             .ReturnsAsync(_fakeRoles);
 
-//            _rolesServiceMock.Setup(r => r.GetAllAsync())
-//                             .ReturnsAsync(_fakeRoles);
+            // Act
+            var result = await _viewComponent.InvokeAsync(selectedRoleName);
 
-//            // Act
-//            var result = await _viewComponent.InvokeAsync(selectedRoleId);
+            // Assert
+            var viewResult = Assert.IsType<ViewViewComponentResult>(result);
+            Assert.Equal("RolesSelection", viewResult.ViewName);
 
-//            // Assert
-//            var viewResult = Assert.IsType<ViewViewComponentResult>(result);
-//            Assert.Equal("RolesSelection", viewResult.ViewName);
+            var model = Assert.IsType<(List<RoleViewModel>, string)>(viewResult.ViewData!.Model);
+            Assert.Equal(_fakeRoles, model.Item1);
+            Assert.Equal(selectedRoleName, model.Item2);
+        }
 
-//            var model = Assert.IsType<(List<RoleViewModel>, Guid)>(viewResult.ViewData!.Model);
-//            Assert.Equal(_fakeRoles, model.Item1);
-//            Assert.Equal(selectedRoleId, model.Item2);
-//        }
+        [Fact]
+        public async Task InvokeAsync_WhenSelectedRoleNotExists_ReturnsViewWithDefaultUserRole()
+        {
+            // Arrange
+            var nonExistentRoleName = Guid.NewGuid().ToString();
+            var defaultRoleName = Constants.UserRoleName;
 
-//        [Fact]
-//        public async Task InvokeAsync_WhenSelectedRoleNotExists_ReturnsViewWithDefaultUserRole()
-//        {
-//            // Arrange
-//            var nonExistentRoleId = Guid.NewGuid();
-//            var defaultRoleId = _fakeRoles.First(r => r.Name == Constants.UserRoleName).Id;
+            _rolesServiceMock.Setup(r => r.GetAllAsync())
+                             .ReturnsAsync(_fakeRoles);
 
-//            _rolesServiceMock.Setup(r => r.GetAllAsync())
-//                             .ReturnsAsync(_fakeRoles);
+            // Act
+            var result = await _viewComponent.InvokeAsync(nonExistentRoleName);
 
-//            // Act
-//            var result = await _viewComponent.InvokeAsync(nonExistentRoleId);
+            // Assert
+            var viewResult = Assert.IsType<ViewViewComponentResult>(result);
+            Assert.Equal("RolesSelection", viewResult.ViewName);
 
-//            // Assert
-//            var viewResult = Assert.IsType<ViewViewComponentResult>(result);
-//            Assert.Equal("RolesSelection", viewResult.ViewName);
-
-//            var model = Assert.IsType<(List<RoleViewModel>, Guid)>(viewResult.ViewData!.Model);
-//            Assert.Equal(_fakeRoles, model.Item1);
-//            Assert.Equal(defaultRoleId, model.Item2);
-//        }
-//    }
-//}
+            var model = Assert.IsType<(List<RoleViewModel>, string)>(viewResult.ViewData!.Model);
+            Assert.Equal(_fakeRoles, model.Item1);
+            Assert.Equal(defaultRoleName, model.Item2);
+        }
+    }
+}
