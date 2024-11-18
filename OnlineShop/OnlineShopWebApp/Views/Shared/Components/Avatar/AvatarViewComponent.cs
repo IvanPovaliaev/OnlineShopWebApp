8a913a6f1a6703cf608fbd6c16d57eec4bcc6658
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-11-19 02:19:44 +0300

    Исправлен баг.

--- OnlineShop/OnlineShopWebApp/Views/Shared/Components/Avatar/AvatarViewComponent.cs
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Avatar/AvatarViewComponent.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 
 namespace OnlineShopWebApp.Views.Shared.Components.Cart
 {
-	public class AvatarViewComponent : ViewComponent
-	{
-		private readonly string _userId;
-		private readonly AccountsService _accountsService;
+    public class AvatarViewComponent : ViewComponent
+    {
+        private readonly string _userId;
+        private readonly AccountsService _accountsService;
 
-		public AvatarViewComponent(AccountsService accountsService, IHttpContextAccessor httpContextAccessor)
-		{
-			_accountsService = accountsService;
-			_userId = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
-		}
+        public AvatarViewComponent(AccountsService accountsService, IHttpContextAccessor httpContextAccessor)
+        {
+            _accountsService = accountsService;
+            _userId = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
 
-		/// <summary>
-		/// Show avatar component on View;
-		/// </summary>
-		/// <returns>AvatarViewComponent</returns>
-		public async Task<IViewComponentResult> InvokeAsync()
-		{
-			var user = await _accountsService.GetAsync(_userId);
-			var avatarUrl = user.AvatarUrl;
-			return View("Avatar", avatarUrl);
-		}
-	}
+        /// <summary>
+        /// Show avatar component on View;
+        /// </summary>
+        /// <returns>AvatarViewComponent</returns>
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _accountsService.GetAsync(_userId);
+            var avatarUrl = user?.AvatarUrl;
+            return View("Avatar", avatarUrl);
+        }
+    }
 }
